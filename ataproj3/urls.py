f5e38234f3d9c7334d0f22365ab21151e6b3d9ef
Radosław Gościniak <bossgos@gmail.com>
2026-04-27 11:37:56 +0200

    Movies - html files - mobile friendly

--- ataproj3/urls.py
+++ ataproj3/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls')),
+    path('', include('movies.urls')),
 ]
 
 
